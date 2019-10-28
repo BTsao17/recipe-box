a565a2f289dd8e8541382e5c588cd9d777621d61
Bonnie Tsao <IceJade1788@gmail.com>
2019-10-28 16:36:12 -0700

    remove react icons and stock css, set up App.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+      </div>
+    );
+  }
 }
 
 export default App;
