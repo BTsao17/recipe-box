980082c8f02d44ef7ea9adba4c0a7ac4a57f9926
Bonnie Tsao <IceJade1788@gmail.com>
2019-10-28 17:39:13 -0700

    set up preliminary routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
@@ -11,11 +11,65 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>Hi</p>
-      </div>
+      <React.Fragment>
+        <header>
+          <h1>Recipe Box</h1>
+        </header>
+        <main>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/appetizers">Appetizers</Link>
+              </li>
+              <li>
+                <Link to="/salads">Salads</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/appetizers">
+              <Appetizers />
+            </Route>
+            <Route path="/salads">
+              <Salads />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+        </main>
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </React.Fragment>
     );
   }
+}
+
+//functional or dummy components
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Appetizers() {
+  return <h2>Appetizers</h2>;
+}
+
+function Salads() {
+  return <h2>Salads</h2>;
+}
+
+function Error() {
+  return (
+    <React.Fragment>
+      <h2>404</h2>
+      <p>The page you are looking for cannot be found.</p>
+    </React.Fragment>
+  );
 }
 
 export default App;
