f9a8903ab0689cb87e0e4938cd50ebe7d979e0c6
Bonnie Tsao <IceJade1788@gmail.com>
2019-10-29 17:02:53 -0700

    set up navbar to create links and paths based on data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -6,42 +6,61 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dishTypes: [],
+    };
+  }
+
+  componentDidMount() {
+    // Promise.all([axios.get('localhost:8080/cuisine'), axios.get('localhost:8080/dishTypes')])
+
+    axios.get('http://localhost:8080/dishTypes').then((response) => {
+      console.log(response);
+      this.setState({
+        dishTypes: response.data,
+      });
+    });
   }
 
   render() {
+    const { dishTypes } = this.state;
+
+    const navBarItems = dishTypes.map((type) => {
+      return (
+        <li key={type}>
+          <Link to={type.toLowerCase()}>{type}</Link>
+        </li>
+      );
+    });
+    const routePaths = dishTypes.map((type) => {
+      return (
+        <Route path={'/' + type.toLowerCase()} key={type}>
+          <RecipeTab type={type} />
+        </Route>
+      );
+    });
+
     return (
       <React.Fragment>
         <header>
           <h1>Recipe Box</h1>
         </header>
+
         <main>
           <nav>
-            <ul>
-              <li>
-                <Link to="/appetizers">Appetizers</Link>
-              </li>
-              <li>
-                <Link to="/salads">Salads</Link>
-              </li>
-            </ul>
+            <ul>{navBarItems}</ul>
           </nav>
-
           <Switch>
             <Route exact path="/">
-              <Home />
+              <HomeTab />
             </Route>
-            <Route path="/appetizers">
-              <Appetizers />
-            </Route>
-            <Route path="/salads">
-              <Salads />
-            </Route>
+            {routePaths}
             <Route path="*">
-              <Error />
+              <ErrorPage />
             </Route>
           </Switch>
         </main>
+
         <footer>
           <p>Footer</p>
         </footer>
@@ -51,19 +70,15 @@ class App extends React.Component {
 }
 
 //functional or dummy components
-function Home() {
+function HomeTab() {
   return <h2>Home</h2>;
 }
 
-function Appetizers() {
-  return <h2>Appetizers</h2>;
+function RecipeTab(props) {
+  return <h2>{props.type}</h2>;
 }
 
-function Salads() {
-  return <h2>Salads</h2>;
-}
-
-function Error() {
+function ErrorPage() {
   return (
     <React.Fragment>
       <h2>404</h2>
--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || process.argv[2] || 8080;
 const path = require('path');
 
+const cors = require('cors');
+
 //data
 const cuisines = require('./data/cuisines');
-const dishTypes = require('./data/dishTypes')
+const dishTypes = require('./data/dishTypes');
 
+app.use(cors()); //for development only
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 console.log(cuisines);
 console.log(dishTypes)
@@ -16,6 +24,10 @@ console.log(dishTypes)
 
 
 
+app.get('/dishTypes', (req, res) => {
+  const data = dishTypes.map((dishType) => dishType.dishType);
+  res.json(data);
+});
 
 app.listen(port, () => {
   console.log(`server listening at ${port}`);
